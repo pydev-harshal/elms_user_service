e4694f772bf6eb58e94eb5c9bfa6d3ac88683e2d
Harshal Marathe <dev.harshalmarathe@gmail.com>
2025-06-05 01:04:57 +0530

    chore(users): enhance user and profile admin with list_display fields

--- users_app/admin.py
+++ users_app/admin.py
@@ -5,6 +5,11 @@ from users_app.models import Profile
 
 User = get_user_model()
 
-admin.site.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('id', 'first_name', 'last_name', 'email', 'is_active', 'is_staff', 'is_superuser')
+admin.site.register(User, UserAdmin)
 
-admin.site.register(Profile)
+
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('user__id', 'user__first_name', 'user__last_name', 'user__email', 'profile_photo')
+admin.site.register(Profile, ProfileAdmin)
