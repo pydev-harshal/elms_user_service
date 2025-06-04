5b36a95b1389aca900c9c42fbd17c7300a102421
Harshal Marathe <dev.harshalmarathe@gmail.com>
2025-06-05 00:59:29 +0530

    feat(users): register user and profile models to admin

--- users_app/admin.py
+++ users_app/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
+from users_app.models import Profile
 
-# Register your models here.
+
+User = get_user_model()
+
+admin.site.register(User)
+
+admin.site.register(Profile)
